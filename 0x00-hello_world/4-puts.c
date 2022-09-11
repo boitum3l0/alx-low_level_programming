246b3a8c79fecf04e11d8bd7274293ab126c1925
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-11 13:14:46 -0700

    Hello, put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,13 +6,16 @@
  *
  *
  *
- * Return: Always 0 (success)     
+ * Return: Always 0 (success)
+ *
  */
 
 int main(void)
 
-{	
+{
+
 	puts("\"Programming is like building a multilingual puzzle");
-	
+
 	return (0);
+
 }
