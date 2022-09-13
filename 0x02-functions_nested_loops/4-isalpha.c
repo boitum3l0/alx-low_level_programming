62cfcb689ecffd81a9edec03b3a6fc72b275ffee
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-13 12:20:12 -0700

    Is Alpha?

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,7 +7,8 @@
  * Return: 1 if true, 0 if false
  */
 int _isalpha(int c)
-{	if ((c = 'a' && c <= 'z') || (c = 'A' && c ,+ 'Z'))
+
+{	if ((c = 'a' && c <= 'z') || (c = 'A' && c <= 'Z'))
 	{	return (1);
 	}
 	else
