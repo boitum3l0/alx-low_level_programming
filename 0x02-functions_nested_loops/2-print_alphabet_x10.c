508cba49ae24005aacb2aead56eb52562b637b78
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-13 09:25:38 -0700

    Alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@ void print_alphabet_x10(void)
 {
 	int i = 1;
 
-	while (1<=10)
+	while (i <= 10)
 	{
-		char = c;
+		char c;
 
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-	_putchar('\n')
+	_putchar('\n');
 	}
 }
