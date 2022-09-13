32900ed65bc5564fc68413267333ca3be4473de9
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-13 11:22:04 -0700

    Lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
- * int _islower(int c) - seeks lowercase chars
+ * int _islower(int c) - looks for lowercase 
+ *
+ * Return: Always 0
  */
+int _islower(int c)
+{	int c
 
+	if (_islower(c))
+	{	return (1);
+	} else
+	{	return (0);
+	}
+
+	_putchar('\n');
+	return (0);
+}
