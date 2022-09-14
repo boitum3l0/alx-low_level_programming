69113255d36a1fa71cd265eabf92180235bc90ed
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-14 12:00:12 -0700

    Print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,13 +15,14 @@ void print_to_98(int n)
 		{	printf("%d, ", n);
 			n++;
 		}
+		printf("%d, ", n);
 	}
 	else
 	{
 		while (n > 98)
-		{	printf("%d ,", n);
+		{	printf("%d, ", n);
 			n--;
 		}
-		printf("%d ,", n);
+		printf("%d, ", n);
 	}
 }
