7d392f31c2754f162193bfe6d2806e2acc4db6e3
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-14 08:28:03 -0700

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * times_table - multiples of 9
+ *
+ * Return: multiples of 9
+ */
+void times_table(void)
+{
+	int nine = o;
+
+	while (nine <= 9)
+	{	nine = (nine *= 9);
+		return (nine)
+		_putchar('\n');
+		nine++;
+	}
+	return (0);
+}
