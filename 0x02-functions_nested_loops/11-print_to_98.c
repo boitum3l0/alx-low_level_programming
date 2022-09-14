4a61ab1ae2b4b40da62ebd297b477bfeb4da4829
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-14 11:36:52 -0700

    Print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,14 +11,16 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (n = 98; n > 98; n--)
+		while (n > 98)
 		{	printf("%d\n", n);
+			n--;
 		}
 	}
 	else if (n < 98)
 	{
-		for (n = 0; n < 98; n++)
+		while (n < 98)
 		{	printf("%d\n", n);
+			n++;
 		}
 	}
 	else
