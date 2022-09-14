7fc9dc97d215df50083d52bea5653eb63ddd39db
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-14 04:54:12 -0700

    HrsnMin

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: every minute of the day
+ */
+void jack_bauer(void)
+{
+	int hrs = 0;
+	int min = 0;
+
+	while (hrs <= 23)
+	{
+		while (min <= 59)
+		{ min += 1;
+		{
+		min ++;
+		hrs +=1;
+	{
+	hrs ++;
+	return (0);
+}
