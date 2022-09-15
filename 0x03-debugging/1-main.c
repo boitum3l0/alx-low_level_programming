7ae348a73c5de5e7ff6a661bf6b5701fc492fb30
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-15 08:22:06 -0700

    Test 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,14 +9,13 @@ int main(void)
 {
 	int i;
 
-	/**
-	 * printf("infinite loop incoming :(\n");
+	printf("infinite loop incoming :(\n");
 
 	i = 0;
-	while (i<10)
-	{
+	/**while (i<10)
+	{*/
 		putchar(i);
-	}
+	/**}
 	*/
 
 	printf("infinite loop avoided! \\o/\n");
