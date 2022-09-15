2cb760eb681daea238dc01c4344c3291d379b450
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-15 08:07:26 -0700

    Test 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,5 +6,12 @@
  * program that tests a function
  */
 void positive_or_negative(int i)
-
+/**
+ * tests largest of 3 integers
+ */
+int largest_number(int a, int b, int c)
+/**
+ * prints remaining days in a year
+ */
+void print_remaining_days(int month, int day, int year)
 #endif
