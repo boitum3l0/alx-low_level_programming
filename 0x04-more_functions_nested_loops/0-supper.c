23396d8d85835175b54885ee946f83fc35641538
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-15 11:37:42 -0700

    T0

--- 0x04-more_functions_nested_loops/0-supper.c
+++ 0x04-more_functions_nested_loops/0-supper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _isupper - checks for uppercase characters
  * @c: character to be checked
@@ -6,7 +7,7 @@
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <+= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
