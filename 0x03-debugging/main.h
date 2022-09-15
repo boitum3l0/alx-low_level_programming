a85d5ff59766bd05922c11b13dfc3fe90d9de4ef
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-15 13:05:08 -0700

    Pos,neg

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,21 +2,9 @@
 #define MAIN_H
 
 #include <stdio.h>
-/**
- * program that tests a function
- */
+
 void positive_or_negative(int i);
-/**
- * tests largest of 3 integers
- */
 int largest_number(int a, int b, int c);
-/**
- * prints remaining days in a year
- */
 void print_remaining_days(int month, int day, int year);
-/**
- * checks positive or negative
- */
-int main(void);
 
 #endif
