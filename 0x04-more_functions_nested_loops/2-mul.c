20083a54b237bdec9c1768527f2a7657469070d5
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-16 01:35:11 -0700

    Mult

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: argument 1
+ * @b: argument 2
+ * Return: the product
+ */
+
+int mul(int a, int b)
+{
+	int product;
+	{
+		product = a * b;
+	}
+	return (product);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,5 +7,7 @@ int _isupper(int c);
 /* checks for uppercase characters*/
 int _isdigit(int c);
 /* checks for digit*/
+int mul(int a, int b);
+/* multiplies two integers*/
 
 #endif
