f8248b0bcd19f7ca01d1e0677a0be5589d921cdd
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-16 01:01:04 -0700

    digits

--- 0x04-more_functions_nested_loops/0-supper.c
+++ 0x04-more_functions_nested_loops/0-supper.c
@@ -1,10 +1,11 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * _isupper - checks for uppercase characters
  * @c: character to be checked
  * Return: always 0
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
