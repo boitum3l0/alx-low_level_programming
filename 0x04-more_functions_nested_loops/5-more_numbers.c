0dfe54e52e738cc948d55874612300cfa7f68cde
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-16 07:27:57 -0700

    Ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10x the number
+ *
+ * Returns: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 14; i++)
+	{
+		while (j <= 10)
+		{
+			_putchar('i');
+		}
+	_putchar("j\n");
+	}
+	_putchar('\n');
+}
