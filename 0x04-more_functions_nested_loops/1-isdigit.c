d09ab788be99eb5bb4144bb1163a321cdf05673c
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-16 00:59:41 -0700

    digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: input to be checked
+ * Return: 1 if digit, 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0'&& c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,8 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-int _isupper(int c);
-/**
- * checks for uppercase characters
- */
 
+int _isupper(int c);
+/* checks for uppercase characters*/
+int _isdigit(int c);
+/* checks for digit*/
+
+#endif
