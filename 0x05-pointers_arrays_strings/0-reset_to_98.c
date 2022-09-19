41da6daa7465b0fcf9a24a2832aeae6907666353
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-19 06:44:28 -0700

    Reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes value of int
+ * @n: pointer
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	_putchar("*n\n");
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(int c);
+/* putchare file to print results*/
+void reset_to_98(int *n);
+/* takes a pointer to an int and updates the value of int*/
+
+#endif
