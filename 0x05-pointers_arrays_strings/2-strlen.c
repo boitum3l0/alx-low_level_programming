592502aff2bbca42baafd86c84546327d9557c23
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-19 08:42:01 -0700

    Strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: character argument
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int arrlen;
+
+	for (arrlen = 0; s[arrlen] != '\0'; arrlen ++)
+	{
+		_puchar(arrlen);
+	}
+	_putchar('\n);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,7 @@ void reset_to_98(int *n);
 /* takes a pointer to an int and updates the value of int*/
 void swap_int(int *a, int *b);
 /* swaps value of two integers*/
+int _strlen(char *s);
+/* function that returns the length of string*/
 
 #endif
