049cab0692f3249f8aa9b958c18bf2293725fb2c
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-20 06:49:02 -0700

    Other

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,15 +7,15 @@
  */
 void puts2(char *str)
 {
-	int o;
+	int a = 0;
 
-	while (str[o] != '\0')
+	while (str[a] != '\0')
 	{
-		if (o % 2 == 0)
+		if (a % 2 == 0)
 		{
-			_putchar(str[o]);
+			_putchar(str[a]);
 		}
-	o++;
+	a++;
 	}
 	_putchar('\n');
 }
