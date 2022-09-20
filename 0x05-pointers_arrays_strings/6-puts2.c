47cd7aadf82008a339ea3e5c120ebc7703c0d89f
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-19 23:35:07 -0700

    Other

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints ever other character
+ * @str: string input
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int o;
+
+	while (str[o] != '\0')
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+			o++;
+		}
+	_putchar('\n');
+}
