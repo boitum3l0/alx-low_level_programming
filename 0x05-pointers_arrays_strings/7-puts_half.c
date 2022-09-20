4570ae15758b3724774b81fbffc1660ea2d51806
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-20 11:57:47 -0700

    Half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,12 @@ void puts_half(char *str)
 	int c = 0;
 	int d = strlen(str);
 	
-	_putchar(str(d) % 2);
+	_putchar(str[d] % 2);
 
 	if (d % 2 != 0)
 	{
 		c = ((d - 1) / 2);
-		_putchar(str(c) % 2);
+		_putchar(str[c] % 2);
 	}
 	
 }
