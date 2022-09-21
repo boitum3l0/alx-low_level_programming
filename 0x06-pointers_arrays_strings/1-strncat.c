8d7276fb9d11110402092c3f2ee41e1ae28094a3
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-21 13:25:14 -0700

    Con2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,8 +12,8 @@ char *_strncat(char *dest, char *src, int n)
 	int a;
 	int b;
 	
-	for (a = 0; i < n && dest[a] != '\0'; a++)
-	for (b = 0; src[b] != '\0'; b++)
+	for (a = 0; dest[a] != '\0'; a++)
+	for (b = 0; b < n && src[b] != '\0'; b++)
 	{
 		dest[a] = src[b];
 	}
