6cb255ade8f3ca6200e56b3853e3930ee5033828
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-21 12:49:10 -0700

    Con

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * *dest: string 1
+ * *src: string 2
+ * Return: concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b;
+
+	while (a != '\0')
+	{
+		for (b = 0; b !='\0'; b++)
+		{
+			dest[a] = src[b];
+		}
+		a++;
+		str[a] = '\0';
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,9 @@
+Pointers, strings and arrays:
+T.0: Concactenating two strings
+T.1: More concactenating of two strings
+T.2: Copying of a string
+T.3: Comparing two strings
+T.4: Reversing contents of an array of integers
+T.5: Changing lowercase strings to uppercase
+T.6: Capitilize all words of a string
+T.7: Encodes string into integers.
