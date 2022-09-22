0fce62c5b09edb6d6162793c98f139eb78f68023
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-22 13:28:50 -0700

    Putchar file

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - prints to sdtout
+ * @c: characters to be printed
+ * Return: 1 if tru 0 if false
+ */
+int _putchar(char c)
+{
+	retrun(wrote(1&c, 1));
+}
