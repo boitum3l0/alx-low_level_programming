ebaad5d6d733d85ddd927db894b011eef2e51614
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-22 14:51:40 -0700

    Cmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if true, 0 if false
+ */
+
+int *_strcmp(char *s1, char *s2)
+{
+	int n = 0;
+
+	while (s1[n] !='\0' && s1[n] != '\0')
+	{
+		 if (s1[n] - s2[n] = 0)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	n++;
+	}
+}
