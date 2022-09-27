4376bcfe00ed1dd4db0b797afd23af463f63a0a6
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-27 09:39:35 -0700

    CPY

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination array
+ * @src: source array
+ * @n: string length
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	while (i < 0)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
