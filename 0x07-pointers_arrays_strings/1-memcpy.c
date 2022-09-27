829e45d853470e23125b64e926c293f6136f5574
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-27 12:43:22 -0700

    Cpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,17 +9,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i = 0;
-
-	if (dest == 0)
+	while (n < 0)
 	{
-		return (NULL);
-	}
-	while (i < 0)
-	{
-		dest[i] = src[i];
-		i++;
+		dest[n] = src[n];
+		n++;
 		return (dest);
 	}
-	return (0);
 }
