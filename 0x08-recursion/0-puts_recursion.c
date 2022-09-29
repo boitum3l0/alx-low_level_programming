bd2fddac4fb121719f0ae447d7b869d77c552bd9
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-29 09:05:54 -0700

    String print

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * puts_recursion - prints a string followed by a new line
+ * @s: string to be printed
+ *
+ * Return: string plus new line
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(s'\n');
+}
