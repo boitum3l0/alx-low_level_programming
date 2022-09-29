4872a39c0b459c0e06fd7bfad889edad620577c7
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-29 11:40:48 -0700

    Len

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,13 +8,13 @@
 
 int _strlen_recursion(char *s)
 {
-	int l = 0;
+	int len = 0;
 	
 	if (*s != '\0')
 	{
-		i++
-		i += _strlen_recursion(s + 1);
+		len++
+		len += _strlen_recursion(s + 1);
 	}
-	return l
+	return (len);
 
 }
