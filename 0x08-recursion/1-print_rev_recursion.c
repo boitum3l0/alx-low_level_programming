9994919e713a56a6fe33e5e13efaaa6747b93417
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-29 10:16:20 -0700

    Rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,11 +10,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar(*s - 1);
-		_print_rev_recursion(s - 1);
-	}
-	else
-	{
-		_putchar('\n');
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
