8240efc2d534cae47a16043882c2df77baedc749
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-29 09:11:42 -0700

    Print string

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,11 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include " _putchar.c"
 #include <stdio.h>
 #include <string.h>
 
 void _puts_recursion(char *s);
+
+int _putchar(char c);
 
 void _print_rev_recursion(char *s);
 
