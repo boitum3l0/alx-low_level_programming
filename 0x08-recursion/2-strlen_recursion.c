5f804ae28d5a9a60f53483e204470aad6f37e815
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-29 11:32:49 -0700

    Len

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
  * _strlen_recursion - returns string length
  * @s: string to check
- *
  * Return: string length
  */
+
 int _strlen_recursion(char *s)
 {
 	int l = 0;
-
-	if (*s[l] != '\0')
+	
+	if *(s[l] != '\0')
 	{
-		_putchar(s[l]);
 		_strlen_recursion(s + 1);
 	}
+	_putchar(s[l]);
+
 }
