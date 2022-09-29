0e8153023e8cca96117c4a3028d50969de438a5c
boitum3l0 <boitumelomonkwe@gmail.com>
2022-09-29 09:59:49 -0700

    Rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: string to reverse
+ * Return: reversed string
+ */
+
+void _print_rev_recursion(char *s);
+{
+	if (*s == '\0')
+	{
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
