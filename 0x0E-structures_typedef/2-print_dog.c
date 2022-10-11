512bf7371aa30a78919e428a765d44f02f9a268f
boitum3l0 <boitumelomonkwe@gmail.com>
2022-10-11 10:46:50 -0700

    Print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,8 +6,6 @@
  */
 void print_dog(struct dog *d)
 {
-	int counter = 0;
-
 	if (d == NULL)
 	{
 		return;
@@ -24,8 +22,6 @@ void print_dog(struct dog *d)
 	{
 		d->owner = "(nil)";
 	}
-	else
-	{
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }
