4ca5aa1ef30e0a397343ab4f43da072d777724a1
boitum3l0 <boitumelomonkwe@gmail.com>
2022-10-25 10:31:27 -0700

    First commit

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,16 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Prototyes will be stored here.
+ */
+
+int _putchar(char c);
+list_t *add_node(list_t **head, const char *str);
+size_t list_len(const list_t *h);
+list_t *add_node_end(list_t **head, const char *str);;
+void free_list(list_t *head);
+
+#endif
