89f7d0d5dc49abf59035f0a4292607a6641132ea
boitum3l0 <boitumelomonkwe@gmail.com>
2022-10-25 07:36:22 -0700

    Eight

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits from 0
+ * Retrun: 0
+ */
+int main(void)
+{
+	int num;
+	char c;
+
+	for (num = 0; num < 10; num++)
+	{
+		puthcar(num + '0');
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
