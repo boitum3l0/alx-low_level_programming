d2e0a23f19cf184c828a696fc69a303bc8b5a25f
Boitumelo Monkwe <boitumelomonkwe@gmail.com>
2022-10-26 00:13:47 +0200

    Nine

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,11 +1,9 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node
- * at a given position.
+ * insert_nodeint_at_index - inserts a new node at a given position.
  * @head: head of a list.
- * @idx: index of the list where the new node is
- * added.
+ * @idx: index of the list where the new node is added.
  * @n: integer element.
  *
  * Return: the address of the new node, or NULL if it
@@ -13,7 +11,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
+	unsigned int j;
 	listint_t *new;
 	listint_t *h;
 
@@ -21,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (i = 0; i < idx - 1 && h != NULL; i++)
+		for (j = 0; j < idx - 1 && h != NULL; j++)
 		{
 			h = h->next;
 		}
