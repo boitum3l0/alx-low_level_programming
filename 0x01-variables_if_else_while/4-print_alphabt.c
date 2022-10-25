2b99ca8e1a804ceedc607b9b4c5cdf2d687cd6cb
boitum3l0 <boitumelomonkwe@gmail.com>
2022-10-25 08:00:58 -0700

    Four

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lower then uppercase except q & e
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha == 'q' && alpha == 'e')
+		{
+			continue;
+			putchar(alpha);
+		}
+	}
+	putchar('\n');
+	return 0;
+}
